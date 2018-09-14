d12f288875882a21add4c62bd6fdffd5e00f3ff8
ternag <terje.nagel@gmail.com>
2018-09-14 21:23:30 +0200

    TwoFer

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,3 +1,6 @@
 ﻿module TwoFer
 
-let twoFer (input: string option): string = failwith "You need to implement this function."
+let twoFer (input: string option): string = 
+    match input with
+    | Some input -> "One for " + input + ", one for me."
+    | None -> "One for you, one for me."
