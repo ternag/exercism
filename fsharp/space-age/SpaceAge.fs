058a1b555605a8664be7fa031685cf766c112279
Terje Nagel <terje.nagel@gmail.com>
2018-09-22 23:23:18 +0200

    space-age attemt 1

---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,27 @@
+﻿module SpaceAge
+
+type Planet = 
+   | Earth
+   | Mercury
+   | Venus
+   | Mars
+   | Jupiter
+   | Saturn
+   | Uranus
+   | Neptune
+
+let earthYear = 31557600.0
+
+let orbitalPeriod (planet:Planet):float =
+    match planet with
+    | Earth -> earthYear
+    | Mercury -> earthYear * 0.2408467
+    | Venus -> earthYear *  0.6151972
+    | Mars -> earthYear *  1.8808158
+    | Jupiter -> earthYear *  11.862615
+    | Saturn -> earthYear *  29.447498
+    | Uranus -> earthYear *  84.016846
+    | Neptune -> earthYear *  164.79132
+
+let age (planet: Planet) (seconds: int64): float = 
+    float seconds / (orbitalPeriod planet)
