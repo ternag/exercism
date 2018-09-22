20200ef9cb269f5080a71827baf817a469576bb2
Terje Nagel <terje.nagel@gmail.com>
2018-09-23 00:07:34 +0200

    difference-of-squares attempt 1

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,12 @@
+﻿module DifferenceOfSquares
+
+let square x = x * x
+
+let squareOfSum (number: int): int = 
+    Seq.sum {1..number} |> square
+
+let sumOfSquares (number: int): int =
+    Seq.sumBy square ({1..number})
+
+let differenceOfSquares (number: int): int =
+    squareOfSum number - sumOfSquares number
