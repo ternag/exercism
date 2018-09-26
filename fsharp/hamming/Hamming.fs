b930e486e5f99822e0a29ccd211ddf3cecbbbeb8
Terje Nagel <terje.nagel@gmail.com>
2018-09-27 01:35:50 +0200

    Another alternative solution to Hamming

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -4,12 +4,24 @@ open Microsoft.FSharp.Core
 let distance a b = 
     match a with
     | _ when Seq.length a <> Seq.length b -> None
-    | _ -> Seq.map2 (fun n m -> if n = m then 0 else 1) a b |> Seq.sum |> Some
+    | _ -> Seq.map2 (fun n m -> if n = m then 0 else 1) a b 
+        |> Seq.sum 
+        |> Some
+
+let distance3 a b = 
+    if (Seq.length a <> Seq.length b) 
+    then None
+    else Seq.map2 (fun n m -> if n = m then 0 else 1) a b 
+      |> Seq.sum 
+      |> Some
 
 let distance2 a b =
     match a with
     | _ when Seq.length a <> Seq.length b -> None
-    | _ -> Seq.zip a b |> Seq.filter (fun (x, y) -> x <> y) |> Seq.length |> Some
+    | _ -> Seq.zip a b 
+        |> Seq.filter (fun (x, y) -> x <> y) 
+        |> Seq.length 
+        |> Some
 
 
 // let distance a b =
