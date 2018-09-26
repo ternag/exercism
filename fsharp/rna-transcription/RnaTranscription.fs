76633def8b7ed3dd81c06dd4040f92c2acab2b93
Terje Nagel <terje.nagel@gmail.com>
2018-09-26 23:43:08 +0200

    Solved RnaTranscription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,13 @@
+﻿module RnaTranscription
+open System
+
+let translate dna =
+    match dna with
+    | 'G' -> 'C'
+    | 'C' -> 'G'
+    | 'T' -> 'A'
+    | 'A' -> 'U'
+    | _ -> '?'
+
+let toRna (dna: string): string = 
+    dna.ToCharArray() |> Array.map translate |> String
