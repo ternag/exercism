0792f88133770d83d20ee9e77dbb4d4a3c9dacf7
Terje Nagel <terje.nagel@gmail.com>
2018-09-27 01:12:28 +0200

    Optimized solution to Hamming

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -4,7 +4,6 @@ open Microsoft.FSharp.Core
 let distance (strand1: string) (strand2: string): int option = 
     match strand1 with
     | _ when strand1.Length <> strand2.Length -> None
-    | _ when strand1 = strand2 -> Some 0
     | _ -> Array.map2 (fun char1 char2 -> if char1 = char2 then 0 else 1) (strand1.ToCharArray()) (strand2.ToCharArray()) 
         |> Array.sum 
         |> Some
@@ -13,11 +12,7 @@ let distance (strand1: string) (strand2: string): int option =
 let distance2 (a:string) (b:string): int option =
     match a with
     | _ when a.Length <> b.Length -> None
-    | _ when a = b -> Some 0
-    | _ -> Seq.zip a b
-        |> Seq.filter (fun (x, y) -> x <> y)
-        |> Seq.length
-        |> Some
+    | _ -> Seq.zip a b |> Seq.filter (fun (x, y) -> x <> y) |> Seq.length |> Some
 
 
 // let distance a b =
