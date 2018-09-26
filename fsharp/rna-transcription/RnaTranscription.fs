6030e3186a792de88f85cb1745a0626e14970ba6
Terje Nagel <terje.nagel@gmail.com>
2018-09-26 23:51:22 +0200

    Changed to use String.collect

--- fsharp/rna-transcription/RnaTranscription.fs
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -3,11 +3,11 @@ open System
 
 let translate dna =
     match dna with
-    | 'G' -> 'C'
-    | 'C' -> 'G'
-    | 'T' -> 'A'
-    | 'A' -> 'U'
-    | _ -> '?'
+    | 'G' -> "C"
+    | 'C' -> "G"
+    | 'T' -> "A"
+    | 'A' -> "U"
+    | _ -> "?"
 
 let toRna (dna: string): string = 
-    dna.ToCharArray() |> Array.map translate |> String
+    String.collect translate dna
