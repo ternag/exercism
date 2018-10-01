e99580cbd7434159af4f6797a24d2eb0d07501ca
Terje Nagel <terje.nagel@gmail.com>
2018-10-01 21:12:37 +0200

    refinement of grains

--- fsharp/grains/Grains.fs
+++ fsharp/grains/Grains.fs
@@ -5,9 +5,15 @@ let square n =
     | x when x > 0 && x <= 64 -> Ok (1UL<<<x - 1)
     | _ -> Error "Invalid input"
 
-let total: Result<uint64,string>= 
-    let t = seq {1..64} |> Seq.map square |> Seq.sumBy (function | Ok n -> n | Error _ -> 0UL) |> uint64
-    Ok t 
+let total: Result<uint64,string>=  
+        [1..64] 
+        |> List.map square 
+        |> List.sumBy (function | Ok n -> n | Error _ -> 0UL) 
+        |> uint64
+        |> Ok 
+
+// let qw str = 
+//     str |> List.filter (fun c -> (not (System.Char.IsDigit c)))
 
 // Valid alternative :o)
 //let total: Result<uint64,string>= 
