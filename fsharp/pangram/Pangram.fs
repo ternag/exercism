0fbdd403f11aacaf0e050bcd2de61efaf502b46c
Terje Nagel <terje.nagel@gmail.com>
2018-10-02 21:39:30 +0200

    Pangram solution

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,13 @@
+﻿module Pangram
+
+open System
+
+let addToListDistinct chr lst =
+    match Char.ToLowerInvariant chr with
+    | c when not (Char.IsLetter c) -> lst
+    | c when List.contains c lst -> lst
+    | c -> c::lst
+
+let isPangram input =
+    let numberOfChars = List.fold (fun acc element -> addToListDistinct element acc ) List.empty (Seq.toList input)
+    numberOfChars.Length = 26
