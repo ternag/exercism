66c7b2c608a2936ef2acc7c72b9aa103bcaf9c6d
Terje Nagel <terje.nagel@gmail.com>
2018-10-03 20:21:29 +0200

    Raindrop solution

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,24 @@
+﻿module Raindrops
+
+let handleDefault (n:int) s = 
+    if(s = "")
+    then n |> string
+    else s
+
+let div3 n s = 
+    if (n % 3 = 0)
+    then s + "Pling"
+    else s
+
+let div5 n s = 
+    if (n % 5 = 0)
+    then s + "Plang"
+    else s
+
+let div7 n s = 
+    if (n % 7 = 0)
+    then s + "Plong"
+    else s
+
+let convert (number: int): string = 
+    div3 number "" |> div5 number |> div7 number |> handleDefault number
